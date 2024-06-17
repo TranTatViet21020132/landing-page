7d7a398f1c7b4530983a7716877a9496106a4fec
TranTatViet21020132 <21020132@vnu.edu.vn>
2024-06-17 15:00:55 +0700

    add responsiveness

--- client/src/components/LoanDetails/LoanDetailCard/LoanDetailCard.tsx
+++ client/src/components/LoanDetails/LoanDetailCard/LoanDetailCard.tsx
@@ -6,23 +6,24 @@ import { LoanDetailCard } from '../../../types/LandingPage/LoanDetail';
 const Card = (props: LoanDetailCard) => {
   const { title, description, hiddenY, delay } = props;
   return (
-    <Reveal hiddenY={hiddenY} delay={delay}>
+    <Reveal hiddenY={hiddenY} delay={delay} width='100%'>
       <div className='loanDetailCard-container'>
         <h4>{title}</h4>
-        {typeof description === "string"
-        ?
-        <ul className='discription-list'>
-          <li>{description}</li>
-        </ul>
-        :
-        <ul className='discription-list'>
-          <li>{description.line1}</li>
-          <li>{description.line2}</li>
-          <li>{description.line3}</li>
-          <li>{description.line4}</li>
-        </ul>
-        }
-
+        <div className="loanDetailCard-content">
+          {typeof description === "string"
+          ?
+          <ul className='discription-list'>
+            <li>{description}</li>
+          </ul>
+          :
+          <ul className='discription-list'>
+            <li>{description.line1}</li>
+            <li>{description.line2}</li>
+            <li>{description.line3}</li>
+            <li>{description.line4}</li>
+          </ul>
+          }          
+        </div>
       </div>
     </Reveal>
 
